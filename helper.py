3bc205946f92d63fa199856b82ee810e62bcd07d
Subhashhari <144612968+Subhashhari@users.noreply.github.com>
2024-04-06 19:31:28 +0530

    Add files via upload

--- helper.py
+++ helper.py
@@ -3,12 +3,14 @@ from IPython import display
 
 plt.ion()
 
-def plot(scores, rolling_avg):
+def plot(scores, mean_scores):
     plt.clf()
     plt.plot(scores, label='Score', color='blue', alpha=0.7, linewidth=1.5, linestyle='-')
-    plt.plot(rolling_avg, label='Rolling_avg', color='orange', alpha=0.7, linewidth=2, linestyle='-')
+    plt.plot(mean_scores, label='Rolling average', color='orange', alpha=0.7, linewidth=2, linestyle='-')
     plt.text(len(scores) - 1, scores[-1], f'{scores[-1]}', ha='center', va='bottom', fontsize=10, color='blue',
              weight='bold')
+    plt.text(len(mean_scores) - 1, mean_scores[-1], f'{mean_scores[-1]:.2f}', ha='center', va='bottom', fontsize=10,
+             color='orange', weight='bold')
     plt.legend(loc='upper left', shadow=True, fontsize='medium')
     plt.tick_params(axis='both', which='major', labelsize=8)
     plt.grid(True, linestyle='--', alpha=0.5)
