2626a13ca78891562097f94cfd7e2f64c3915246
Subhashhari <144612968+Subhashhari@users.noreply.github.com>
2024-04-06 18:27:16 +0530

    Add files via upload

--- helper.py
+++ helper.py
@@ -3,13 +3,13 @@ from IPython import display
 
 plt.ion()
 
-def plot(scores, mean_scores):
+def plot(scores, rolling_avg):
     plt.clf()
-    plt.plot(scores, label='Score', color='blue', alpha=0.7, linewidth=1.5, linestyle='-')
-    plt.plot(mean_scores, label='Mean Score', color='orange', alpha=0.7, linewidth=2, linestyle='-')
+    plt.plot(scores, label='Score', color='cornflowerblue', alpha=0.7, linewidth=1.5, linestyle='-')
+    plt.plot(rolling_avg, label='Rolling_avg', color='orange', alpha=0.7, linewidth=2, linestyle='-')
     plt.text(len(scores) - 1, scores[-1], f'{scores[-1]}', ha='center', va='bottom', fontsize=10, color='blue',
              weight='bold')
-    plt.text(len(mean_scores) - 1, mean_scores[-1], f'{mean_scores[-1]:.2f}', ha='center', va='bottom', fontsize=10,
+    plt.text(len(rolling_avg) - 1, rolling_avg[-1], f'{rolling_avg[-1]:.2f}', ha='center', va='bottom', fontsize=10,
              color='orange', weight='bold')
     plt.legend(loc='upper left', shadow=True, fontsize='medium')
     plt.tick_params(axis='both', which='major', labelsize=8)
@@ -17,7 +17,7 @@ def plot(scores, mean_scores):
     plt.title('Training Progress', fontsize=14)
     plt.xlabel('Number of Games', fontsize=10)
     plt.ylabel('Score', fontsize=10)
-    plt.ylim(ymin=0)
+    plt.ylim(0, max(max(scores), max(rolling_avg)) * 1.1)
     plt.tight_layout()
     display.clear_output(wait=True)
     display.display(plt.gcf())
